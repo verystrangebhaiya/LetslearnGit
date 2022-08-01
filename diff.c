67f1ba318dfb5bca98787c1e212370b3d77805f5
verystrangebhaiya <101050163+verystrangebhaiya@users.noreply.github.com>
2022-08-01 15:05:49 +0530

    Add files via upload

---
+++ diff.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+    int n =10,m=5;
+    printf("Difference of numbers is: %d", (n-m));
+}
---
+++ sum.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+    int n =10,m=5;
+    printf("Sum of numbers is: %d", (n+m));
+}
